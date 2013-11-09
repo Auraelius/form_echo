6bc51b75bf97e67a3e4aa5f217d9e8f1789ca4df
Al Zimmerman <auraelius@gmail.com>
2013-11-09 11:40:18 -0800

    form echo page, switched to erb

--- web.rb
+++ web.rb
@@ -1,41 +1,105 @@
 require 'sinatra'
 
+
 get '/' do
-  haml :index
+  erb :index
 end
 
 post '/form-echo' do
-  haml :form-echo
+  erb :form_echo
 end
 
 __END__
 
 @@ layout
-%html
-  = yield
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Form Echo</title>
+  <style>
+
+  body {font-family: sans-serif;}
+  li {margin: 20px;}
+  .button {float:right;}
+  fieldset {width:30%;}
+  table {
+    border: 1px solid black; 
+    width:80%;
+    }
+  td{
+       border: 1px solid black; 
+       padding: 20px;
+       margin: 10px;
+  }
+
+
+
+  </style>
+
+
+</head>
+<body>
+
+  <div class="page">
+    <%= yield%>
+  </div>
+
+
+</body>
+</html>
 
 @@ index
-%div.title Example Form:
+<h1> Example Form</h1>
 
-  %p Enter anything in this form and click the 
-  send button to see the field names and values 
-  echoed to your browser. Look at this page source 
-  to see where to POST your form to echo its contents.
-  
-  %form{ :action => "http://form-echo.herokuapp.com/form-echo", :method => "post"}
-    %fieldset
-      %ol
-        %li
-          %label{:for => "name"} Name:
-          %input{:type => "text", :name => "name", :class => "text"}
-        %li
-          %label{:for => "mail"} email:
-          %input{:type => "text", :name => "mail", :class => "text"}
-        %li
-          %label{:for => "body"} Message:
-          %textarea{:name => "body"}
-      %input{:type => "submit", :value => "Send", :class => "button"}
+<p> Enter anything in this form and click the 
+send button to see the field names and values 
+echoed to your browser. Look at this page source 
+to see where to POST your form to echo its contents.</p>
+
+<form action= "http://form-echo.herokuapps.com/form-echo", method = "post">
+  <fieldset>
+    <ol>
+      <li>
+        <label for = "name"> Name: </label><br />
+        <input type = "text", name ="name"></input>
+        </li>
+      <li>
+        <label for = "mail"> Email: </label><br />
+        <input type = "text", name ="mail"></input>
+      <li>
+        <label for = "body"> Message: </label><br />
+        <textarea name = "body"></textarea>
+        </li>
+        </ol>
+    <input class="button" type = "submit", value = "Send"></input>
+  </fieldset>
+
+</form>
 
 
-@@ form-echo
-%div.title Form fields (names and values):
+@@ form_echo
+<h1> Form fields from the params hash:</h1>
+
+<table>
+  <tr>
+    <th>Key</th>
+    <th>Value</th>
+  </tr>
+  <% params.each do |key,value| %>
+    <tr>
+      <td style ="text-align:right; background-color: lightgray;">
+        <%= key.to_s %>
+      </td>
+      <td>
+        <%= value.to_s %>
+      </td>
+    </tr>
+  <% end %>
+
+</table>
+
+
+
+
+
+
